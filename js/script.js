4646c2d9c75c36c9eef9216a4b833773f52a5391
Robertokello11 <robert.otieno@student.moringaschool.com>
2021-05-03 16:23:55 +0300

    add all changes

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var pos = 0,test,test_status,question,choice,choices,chA,chB,chC,chD,chE,correct = 0;
+var questions = [
+        [ "The script tag must be placed in", "head" ,"head and body", "title and head" ,"all of the mentioned", "B"],
+        [ "JavaScript is ideal to", "make computations in HTML simpler", "minimize storage requirements on the web server", "increase the download time for the client", "none of the mentioned", "B"],
+        [ "JavaScript Code can be called by using", "RMI", "Triggering Event", "Preprocessor", "Function/Method", "D"],
+        [ "JavaScript can be written", "directly into JS file and included into HTML", "directly on the server page", "directly into HTML pages", "all of the mentioned", "A"],
+        [ "Which of the following Attribute is used to include External JS code inside your HTML Document", "src", "ext", "script", "link", "A"]
+];
+function _(x) {
+     return document.getElementById(x);
+}
+function renderQuestion() {
+    test = _("test");
+    _("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+    question = questions[pos] [0];
+     chA = questions[pos] [1];
+     chB = questions[pos] [2];
+     chC = questions[pos] [3];
+     chD = questions[pos] [4];
+     chE = questions[pos] [5];
+     test.innerHTML = "<h3>"+question+"</h3>";
+     test.innerHTML += "<input type='radio' name='choices' value='A'> "+chB+"<br>";
+     test.innerHTML += "<input type='radio' name='choices' value='B'> "+chB+"<br>";
+     test.innerHTML += "<input type='radio' name='choices' value='C'> "+chD+"<br>";
+     test.innerHTML += "<input type='radio' name='choices' value='D'> "+chA+"<br>";
+     test.innerHTML += "<input type='radio' name='choices' value='E'> "+chA+"<br>";
+     test.innerHTML += "<button onclick='checkAnswer()'>submit Answer</button>";
+}
+function checkAnswer() {
+   choices = document.getElementsByName("choices");
+   for(var i=0; i<)
+}
