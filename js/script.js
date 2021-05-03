62bdcb4ece17d5575e8bc35cbb863a518830a10a
Robertokello11 <robert.otieno@student.moringaschool.com>
2021-05-03 18:30:50 +0300

    make changes appear

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 var pos = 0,test,test_status,question,choice,choices,chA,chB,chC,chD,chE,correct = 0;
 var questions = [
-        [ "The script tag must be placed in", "head" ,"head and body", "title and head" ,"all of the mentioned", "B"],
-        [ "JavaScript is ideal to", "make computations in HTML simpler", "minimize storage requirements on the web server", "increase the download time for the client", "none of the mentioned", "B"],
-        [ "JavaScript Code can be called by using", "RMI", "Triggering Event", "Preprocessor", "Function/Method", "D"],
-        [ "JavaScript can be written", "directly into JS file and included into HTML", "directly on the server page", "directly into HTML pages", "all of the mentioned", "A"],
-        [ "Which of the following Attribute is used to include External JS code inside your HTML Document", "src", "ext", "script", "link", "A"]
+        [ "The script tag must be placed in?", "head" ,"head and body", "title and head" ,"all of the mentioned", "B"],
+        [ "JavaScript is ideal to?", "make computations in HTML simpler", "minimize storage requirements on the web server", "increase the download time for the client", "none of the mentioned", "B"],
+        [ "JavaScript Code can be called by using?", "RMI", "Triggering Event", "Preprocessor", "Function/Method", "D"],
+        [ "JavaScript can be written?", "directly into JS file and included into HTML", "directly on the server page", "directly into HTML pages", "all of the mentioned", "A"],
+        [ "Which of the following Attribute is used to include External JS code inside your HTML Document?", "src", "ext", "script", "link", "A"]
 ];
 function _(x) {
      return document.getElementById(x);
@@ -27,7 +27,6 @@ function renderQuestion() {
      chB = questions[pos] [2];
      chC = questions[pos] [3];
      chD = questions[pos] [4];
-     chE = questions[pos] [5];
      test.innerHTML = "<h3>"+question+"</h3>";
      test.innerHTML += "<input type='radio' name='choices' value='A'> "+chA+"<br>";
      test.innerHTML += "<input type='radio' name='choices' value='B'> "+chB+"<br>";
